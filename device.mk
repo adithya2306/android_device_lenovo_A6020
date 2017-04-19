80b57069f42ccdf493da4c980c4514dfdd41555b
Michael Schwartz <schwartzmi@google.com>
2017-04-19 14:07:38 -0700

    A6020: Declare location of device manifest
    Bug: 36810913
    Bug: 36983271
    Test: make and inspect $OUT/vendor/manifest.xml
    Change-Id: I2a3a6adc7193d95875729e93a987cb7c11aab7aa

--- device.mk
+++ device.mk
@@ -120,9 +120,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
 
 # HIDL
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
-
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0

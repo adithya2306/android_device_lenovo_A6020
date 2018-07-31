63e2f157a4314f9fb9d72b01fbf58c991a0e3951
Bruno Martins <bgcngm@gmail.com>
2018-07-31 17:07:24 +0200

    A6020: Import nubia offline charger
     * from nx512j
     * preview: https://imgur.com/a/t9aa9a0
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -59,3 +59,14 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+# Offline charging
+include $(CLEAR_VARS)
+LOCAL_MODULE          := poweroffcharge
+LOCAL_MODULE_OWNER    := lenovo
+LOCAL_MODULE_PATH     := $(TARGET_ROOT_OUT_SBIN)
+LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_SBIN_UNSTRIPPED)
+LOCAL_SRC_FILES       := sbin/poweroffcharge
+LOCAL_MODULE_TAGS     := optional
+LOCAL_MODULE_CLASS    := EXECUTABLES
+include $(BUILD_PREBUILT)

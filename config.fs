ce1e284e5209d57891f96a0602d07b0a20808631
Bruno Martins <bgcngm@gmail.com>
2018-10-08 00:25:18 +0100

    A6020: Create root folders outside of vendor init script
     * The core SELinux policies for vendor_init are being restricted as to
       comply with Treble. Simply adding a local rule to allow creating
       folders under rootfs would be enough, but at the same time defining
       the extra root folders and making use of AID/fs configuration file
       to set perms is more neat and clean.
    
    Change-Id: Idabd7ae72e4c4fd9daac5ba3be3e6236f79f251b

--- config.fs
+++ config.fs
@@ -18,3 +18,15 @@ mode: 0755
 user: AID_SYSTEM
 group: AID_RADIO
 caps: NET_BIND_SERVICE
+
+[firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[persist/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0

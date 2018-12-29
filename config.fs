a22ace0d1a31791a3ccfe63986ddb9657d61aeb8
Brandon Solis <bsolis@codeaurora.org>
2018-12-28 16:06:15 -0800

    A6020: Update user and group for IMS daemons
    Move all ims daemons to radio user and group
    
    Change-Id: I8ee1c4ff182af0d9b4ebf028e3096cfa88118287

--- config.fs
+++ config.fs
@@ -21,12 +21,12 @@ caps: 0
 
 [system/vendor/bin/imsdatadaemon]
 mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
+user: AID_RADIO
+group: AID_RADIO
 caps: NET_BIND_SERVICE
 
 [system/vendor/bin/ims_rtp_daemon]
 mode: 0755
-user: AID_SYSTEM
+user: AID_RADIO
 group: AID_RADIO
 caps: NET_BIND_SERVICE

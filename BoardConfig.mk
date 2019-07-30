a9261ed1ab0e2d88595d39f73845f8b0a52dc970
Adithya R <gh0strider.2k18.reborn@gmail.com>
2019-07-30 22:51:57 +0530

    A6020: Set camera binaries' API level to 22 to allow text relocations
    - Some of our camera blobs still use text relocations, which are strictly not
      allowed in API level 23:
    
    07-30 01:15:03.849 25131 25131 E linker  : "/system/vendor/lib/libmmcamera_faceproc.so" has text relocations (https://android.googlesource.com/platform/bionic/+/master/android-changes-for-ndk-developers.md#Text-Relocations-Enforced-for-API-level-23)
    07-30 01:15:03.849 25131 25131 E mm-camera-img: faceproc_comp_eng_load:100] Error opening libmmcamera_faceproc.so lib
    07-30 01:15:03.849 25131 25131 E mm-camera: module_faceproc_init:1583] Error rc -1
    ...

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,8 +69,8 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 TARGET_HAS_LEGACY_CAMERA_HAL1 := true
 TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
 TARGET_PROCESS_SDK_VERSION_OVERRIDE := \
-	/system/bin/mediaserver=23 \
-	/system/vendor/bin/mm-qcamera-daemon=23
+	/system/bin/mediaserver=22 \
+	/system/vendor/bin/mm-qcamera-daemon=22
 
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true

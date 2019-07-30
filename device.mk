022d0fe13d12c49aec0c88ad4d5e8b5f187ad3c0
Adithya R <gh0strider.2k18.reborn@gmail.com>
2019-07-30 15:04:58 +0530

    A6020: Build audio@2.0 HIDL service

--- device.mk
+++ device.mk
@@ -36,6 +36,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl \
+    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@4.0-impl \
     android.hardware.broadcastradio@1.0-impl \
     android.hardware.soundtrigger@2.0-impl

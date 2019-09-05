1c122540bd928a8a6b80c28de9b33757e5830f26
Alessandro Astone <ales.astone@gmail.com>
2019-09-05 22:17:57 +0200

    A6020: Create /persist and /firmware mountpoints via build system
     * With System As Root / is a read-only filesystem
       so we can't create our mountpoints in init anymore
    
    Change-Id: I513b5d47ec0703aef65065a7ff26d5ad941384e7

--- config.fs
+++ config.fs
@@ -18,3 +18,15 @@ mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
+
+[firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[persist/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0

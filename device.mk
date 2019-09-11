eae23e4f3be9a3cc4dfbe31ea3684ed0feb4ad42
Adithya R <gh0strider.2k18.reborn@gmail.com>
2019-09-11 06:48:37 +0530

    A6020: Update HIDLs and manifest
    * Add manifest entries for all HIDLs we're currently shipping
    * Binderize some HIDLs
    * Nuke thermal HIDL as its useless without QCOM thermal HAL

--- device.mk
+++ device.mk
@@ -238,8 +238,8 @@ PRODUCT_PACKAGES += \
 #Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl \
+    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@4.0-impl \
-    android.hardware.broadcastradio@1.0-impl \
     android.hardware.soundtrigger@2.0-impl
 
 # Camera
@@ -254,6 +254,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service
 
 # DRM
 PRODUCT_PACKAGES += \
@@ -263,6 +264,7 @@ PRODUCT_PACKAGES += \
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-service
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
@@ -282,11 +284,7 @@ PRODUCT_PACKAGES += \
 
 #Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
-
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
+    android.hardware.sensors@1.0-impl
 
 #Wifi
 PRODUCT_PACKAGES += \
@@ -298,6 +296,5 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
-
-
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service

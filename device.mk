ac78056beadbeee69c7eef93a7f306a6c46639f8
Davide Garberi <dade.garberi@gmail.com>
2019-09-29 11:59:57 +0200

    A6020: Uprev QTI power HAL to v1.2
    Change-Id: I2d5d4b24def70a027a04fc81fa168fb698280a98

--- device.mk
+++ device.mk
@@ -323,7 +323,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service-qti
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \

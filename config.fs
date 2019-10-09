cdce28ffedc221e97653cb72b3e86162a396fa7e
Adithya R <gh0strider.2k18.reborn@gmail.com>
2019-10-09 17:13:37 +0530

    A6020: Bring back IMS support
     * Disabled VT and WFC as they don't work anyway
     * Credits to @eErenYeager for adapting our legacy IMS stack for pie
    
    This reverts commit ac3af9d.

--- config.fs
+++ config.fs
@@ -18,3 +18,15 @@ mode: 0771
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: 0
+
+[system/vendor/bin/imsdatadaemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[system/vendor/bin/ims_rtp_daemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: NET_BIND_SERVICE

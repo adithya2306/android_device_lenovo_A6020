39f653433df49d6205b09e6dcdb6f76b1b049dd2
Adithya R <gh0strider.2k18.reborn@gmail.com>
2019-12-19 07:50:43 +0530

    A6020: Disable HW FDE for now
     * We are currently using software keymaster implementation, so let's hold FDE until
       we use binderized keymaster + keystore HAL

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,9 +93,8 @@ TARGET_ENABLE_MEDIADRM_64 := true
 TARGET_TAP_TO_WAKE_NODE := "/sys/android_touch/doubletap2wake"
 
 # Crypto
-TARGET_HW_DISK_ENCRYPTION := true
+#TARGET_HW_DISK_ENCRYPTION := true
 TARGET_KEYMASTER_WAIT_FOR_QSEE := true
-
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072

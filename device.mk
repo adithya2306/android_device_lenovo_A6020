0e28f5fe9bb71b65d5a50faaf4e3bb25de0cc48a
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2020-01-08 00:06:25 +0100

    A6020: Build ParanoidDoze
    Change-Id: I15aa87442124d30d6f69eb8d2c10ce47d26475d5

--- device.mk
+++ device.mk
@@ -173,6 +173,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:system/vendor/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:system/vendor/etc/usb_audio_policy_configuration.xml
 
+# ParanoidDoze
+PRODUCT_PACKAGES += \
+    ParanoidDoze
+
 # Privapp permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-lineage.xml:system/etc/permissions/privapp-permissions-lineage.xml

3c397d16eb1759885e55e465b7a2ff022e657d5d
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-01-17 07:47:01 +0530

    A6020: Switch to PA's power HAL

--- device.mk
+++ device.mk
@@ -353,7 +353,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power@1.2-service
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \

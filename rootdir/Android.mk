3b24dfed9bf6077cb0bfe62df9063b42f2b96ae7
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-03-18 19:23:39 +0530

    A6020: Nuke nubia offline charger
    - let's use the dope pixel offline charging animation in PA

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -71,14 +71,3 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.safailnet.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
-
-# Offline charging
-include $(CLEAR_VARS)
-LOCAL_MODULE          := poweroffcharge
-LOCAL_MODULE_OWNER    := lenovo
-LOCAL_MODULE_PATH     := $(TARGET_ROOT_OUT_SBIN)
-LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_SBIN_UNSTRIPPED)
-LOCAL_SRC_FILES       := sbin/poweroffcharge
-LOCAL_MODULE_TAGS     := optional
-LOCAL_MODULE_CLASS    := EXECUTABLES
-include $(BUILD_PREBUILT)

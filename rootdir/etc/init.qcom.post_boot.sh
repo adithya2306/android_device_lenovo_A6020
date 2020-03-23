83e97467b3bc53352204bfff0b089516c6add145
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-03-23 10:26:37 +0530

    A6020: post_boot: Make cpu0 and cpu1 always online in sd616
    - core_control unnecessarily switches between any 2 cpus at random times,
      this can add to power consumption. Keep first 2 cores always online instead
      and power up the other 2 depending on load

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -949,9 +949,9 @@ case "$target" in
                 echo 50000 > /proc/sys/kernel/sched_freq_dec_notify
 
                 # Enable core control
-#                insmod /system/lib/modules/core_ctl.ko
                 echo 2 > /sys/devices/system/cpu/cpu0/core_ctl/min_cpus
                 echo 4 > /sys/devices/system/cpu/cpu0/core_ctl/max_cpus
+                echo "1 1 0 0" > /sys/devices/system/cpu/cpu0/core_ctl/always_online_cpu
                 echo 68 > /sys/devices/system/cpu/cpu0/core_ctl/busy_up_thres
                 echo 40 > /sys/devices/system/cpu/cpu0/core_ctl/busy_down_thres
                 echo 100 > /sys/devices/system/cpu/cpu0/core_ctl/offline_delay_ms
